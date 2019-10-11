3eaa24a10c4ff07a9f366bdb8f8dd66afbe7fcdb
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-10-11 16:05:00 +0100

    Homepage hero section and placeholder text

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-const Home = () => <p>This is the Home Page</p>;
+const Home = () => (
+  <section class="hero is-white is-large is-bold" id="homepage-hero">
+    <div class="hero-body">
+      <div class="container">
+        <nav class="level">
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading is-black">Welcome to</p>
+              <p class="title">The Flower Room</p>
+              <p class="heading is-black">Barnes</p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </section>
+);
 
 export default Home;
